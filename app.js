cedf16795393df52921a89dd39747de82b7e0b5f
mansi_5543 <mansisanaytamboli@gmail.com>
2026-09-02 15:19:02 +0530

    feat: add dynamic study plan calculation

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("Day 1 starts now");
+// function to calculate required daily study hours
+function calculateStudyPlan(targetWeeks, targetTotalHours) {
+    const totalDays = targetWeeks *7
+    const hoursperDay = (targetTotalHours / totaldays).toFixed(1);
+     
+    return {
+        totalDays,
+        dailyRequirement: hoursperDay
+    };
+}
+ const developer = "Man                     si";
+ const plan = calculateStudyPlan(16,800);
+
+ console.log("----------------------------------------");
+ console.log('Developer: ${developer}');
+ console.log('Timeline: ${plan.totalDays}Days');
+ console.log('Commitment:${plan.dailyRequiremnet} hours/day to hit 800 hours');
+ console.log("Status in progress");
+ console.log("-----------------------------------------------");
+ 
